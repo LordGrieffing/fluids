f335c9bea70b51bcb000e268591693bbee49dd11
Jacob Dent <murkymicrobe88@gmail.com>
2025-05-01 15:25:32 -0400

    Changed field names for cell class. Changed field types from float to double for cell class. added getCell and get Delta functions for the particle class.

---
+++ simpleSim/fluid.cpp
@@ -0,0 +1,81 @@
+#include <cmath>
+#include <array>
+#include <vector>
+#include "fluid.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Update the paticles current position and velocity
+void particle::updatePart(const std::array<double,2>& acc, double dt){
+    position = addArray(position, arrayScale(velocity, dt));
+    velocity = addArray(velocity, arrayScale(acc, dt));
+}
+
+// Get the cell that the particle is currently in
+std::array<int, 2> particle::getCell(double height){
+    int cellX = floor((position[0]/height));
+    int cellY = floor((position[1]/height));
+
+    std::array<int, 2> cell = {cellX, cellY};
+    return cell;
+}
+
+// Get the delta of the x and y for a particle's position
+std::array<double, 2> particle::getDelta(const std::array<int, 2>& cell, double height){
+    double deltaX = position[0] - (cell[0] * height);
+    double deltaY = position[1] - (cell[1] * height);
+
+    std::array<double, 2> delta = {deltaX, deltaY};
+    return delta;
+}
+
+
+
+
+
+
+
+
+
+// Finds the magnitude of an array
+double magnitude(const std::array<double, 2>& vec){
+    return sqrt(pow(vec[0], 2) + pow(vec[1], 2));
+}
+
+// Function used to add two position arrays or velocity arrays
+std::array<double, 2> addArray(const std::array<double, 2>& arr1, const std::array<double, 2>& arr2){
+    
+    std::array<double, 2> arrTot;
+
+    for (int i = 0; i < 2; i++){    
+        arrTot[i] = arr1[i] + arr2[i];
+    }
+
+    return arrTot;
+}
+
+// multiply an array by a scalar
+std::array<double, 2> arrayScale(const std::array<double, 2>& arr1, double scalar){
+
+    std::array<double, 2> arrTot;
+
+    for(int i = 0; i < 2; i++){
+        arrTot[i] = arr1[i] * scalar;
+    }
+    return arrTot;
+}
--- simpleSim/fluid.h
+++ simpleSim/fluid.h
@@ -5,17 +5,25 @@
 #include <cmath>
 
 
+// helper functions
+double magnitude(const std::array<double, 2>& vec);
+std::array<double, 2> addArray(const std::array<double, 3>& arr1, const std::array<double, 3>& arr2);
+std::array<double, 2> arrayScale(const std::array<double, 3>& arr1, double scalar);
+
+
 // Cell object for a grid
 class cell{
     public:
 
         // Public attributes
-        std::array<double, 2> center;
-        float size;
-        float velX;
-        float velY;
-        float pressure;
+        std::array<int, 2> coordinates;
+        double height;
+        double velX;
+        double velY;
+        double pressure;
         int type;
+
+        // Public methods
 
 };
 
@@ -26,4 +34,10 @@ class particle{
         // Public attributes
         std::array<double, 2> velocity;
         std::array<double, 2> position;
+
+
+        // Public functions
+        void updatePart(const std::array<double, 2>& acc, double dt);
+        std::array<int, 2> getCell(double height);
+        std::array<double, 2> getDelta(const std::array<int, 2>& cell, double height);
 };
